1741fc12a8b3a7aff84de976aa2222f29e915314
64epicks <jacob_paul@outlook.com>
2019-10-03 13:33:57 +0200

    Added functions.o to arch X86_64

--- arch/X86_64/functions.asm
+++ arch/X86_64/functions.asm
@@ -3,18 +3,18 @@ section .text
 
 global CPUID_enabled
 CPUID_enabled:
-    pushfd                               ;Save EFLAGS
-    pushfd                               ;Store EFLAGS
-    xor dword [esp],0x00200000           ;Invert the ID bit in stored EFLAGS
-    popfd                                ;Load stored EFLAGS (with ID bit inverted)
-    pushfd                               ;Store EFLAGS again (ID bit may or may not be inverted)
-    pop eax                              ;eax = modified EFLAGS (ID bit may or may not be inverted)
-    xor eax,[esp]                        ;eax = whichever bits were changed
-    popfd                                ;Restore original EFLAGS
-    and eax,0x00200000                   ;eax = zero if ID bit can't be changed, else non-zero
-    cmp eax, 0
+    pushfq                               ;Save RFLAGS
+    pushfq                               ;Store RFLAGS
+    xor qword [esp], 0x00200000          ;Invert the ID bit in stored RFLAGS
+    popfq                                ;Load stored RFLAGS (with ID bit inverted)
+    pushfq                               ;Store RFLAGS again (ID bit may or may not be inverted)
+    pop rax                              ;rax = modified RFLAGS (ID bit may or may not be inverted)
+    xor rax, [esp]                       ;rax = whichever bits were changed
+    popfq                                ;Restore original RFLAGS
+    and rax, 0x00200000                  ;rax = zero if ID bit can't be changed, else non-zero
+    cmp rax, 0
     jne .true
     ret
     .true:
-        mov eax, 1
+        mov rax, 1
         ret

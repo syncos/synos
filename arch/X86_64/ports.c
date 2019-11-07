6962a9e4606bff20bd5f554eee1c26b17b2e80a6
Jacob Paul <jacob_paul@outlook.com>
2019-11-07 11:02:37 +0100

    -Added atomic locks to port functions

--- arch/X86_64/ports.c
+++ arch/X86_64/ports.c
@@ -1,13 +1,44 @@
 #include <mkos/arch/io.h>
 #include <inttypes.h>
 
+char  portLocked[65536];
+bool portInit = false;
+
+int port_compare_exchange(char* ptr, char compare, char exchange)
+{
+    return __atomic_compare_exchange_n(ptr, &compare, exchange, 0, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+}
+void port_store(char* ptr, char value)
+{
+    __atomic_store_n(ptr, value, __ATOMIC_SEQ_CST);
+}
+void initPorts()
+{
+    for (int i = 0; i < 65536; i++) portLocked[i] = 0;
+    portInit = true;
+}
+void port_lock(uint32_t port)
+{
+    if(!portInit) initPorts();
+    while (!port_compare_exchange(&portLocked[port], 0, 1)) {}
+}
+void port_unlock(uint32_t port)
+{
+    if(!portInit) initPorts();
+    port_store(&portLocked[port], 0);
+}
+
 uint8_t inb(uint32_t port)
 {
+    port_lock(port);
     uint8_t ret;
     asm volatile ("inb al, dx" : "=r" (ret): "r" (port));
     return ret;
+    port_unlock(port);
 }
 void outb(uint32_t port, uint8_t value)
 {
+    port_lock(port);
     asm volatile ("outb dx, al": :"d" (port), "a" (value));
+    port_unlock(port);
 }
--- mutils/include/stdatomic.h
+++ mutils/include/stdatomic.h
@@ -3,6 +3,8 @@
 #ifndef _STDATOMIC_H
 #define _STDATOMIC_H
 
+#include <inttypes.h>
+
 /**
  * @file
  *
@@ -10,5 +12,44 @@
  *
  * @see http://libc11.org/stdatomic/
  */
+
+typedef _Atomic _Bool atomic_bool;
+typedef _Atomic char atomic_char;
+typedef _Atomic signed char atomic_schar;
+typedef _Atomic unsigned char atomic_uchar;
+typedef _Atomic short atomic_short;
+typedef _Atomic unsigned short atomic_ushort;
+typedef _Atomic int atomic_int;
+typedef _Atomic unsigned int atomic_uint;
+typedef _Atomic long atomic_long;
+typedef _Atomic unsigned long atomic_ulong;
+typedef _Atomic long long atomic_llong;
+typedef _Atomic unsigned long long atomic_ullong;
+typedef _Atomic __CHAR16_TYPE__ atomic_char16_t;
+typedef _Atomic __CHAR32_TYPE__ atomic_char32_t;
+typedef _Atomic __WCHAR_TYPE__ atomic_wchar_t;
+typedef _Atomic __INT_LEAST8_TYPE__ atomic_int_least8_t;
+typedef _Atomic __UINT_LEAST8_TYPE__ atomic_uint_least8_t;
+typedef _Atomic __INT_LEAST16_TYPE__ atomic_int_least16_t;
+typedef _Atomic __UINT_LEAST16_TYPE__ atomic_uint_least16_t;
+typedef _Atomic __INT_LEAST32_TYPE__ atomic_int_least32_t;
+typedef _Atomic __UINT_LEAST32_TYPE__ atomic_uint_least32_t;
+typedef _Atomic __INT_LEAST64_TYPE__ atomic_int_least64_t;
+typedef _Atomic __UINT_LEAST64_TYPE__ atomic_uint_least64_t;
+typedef _Atomic __INT_FAST8_TYPE__ atomic_int_fast8_t;
+typedef _Atomic __UINT_FAST8_TYPE__ atomic_uint_fast8_t;
+typedef _Atomic __INT_FAST16_TYPE__ atomic_int_fast16_t;
+typedef _Atomic __UINT_FAST16_TYPE__ atomic_uint_fast16_t;
+typedef _Atomic __INT_FAST32_TYPE__ atomic_int_fast32_t;
+typedef _Atomic __UINT_FAST32_TYPE__ atomic_uint_fast32_t;
+typedef _Atomic __INT_FAST64_TYPE__ atomic_int_fast64_t;
+typedef _Atomic __UINT_FAST64_TYPE__ atomic_uint_fast64_t;
+typedef _Atomic __INTPTR_TYPE__ atomic_intptr_t;
+typedef _Atomic __UINTPTR_TYPE__ atomic_uintptr_t;
+typedef _Atomic __SIZE_TYPE__ atomic_size_t;
+typedef _Atomic __PTRDIFF_TYPE__ atomic_ptrdiff_t;
+typedef _Atomic __INTMAX_TYPE__ atomic_intmax_t;
+typedef _Atomic __UINTMAX_TYPE__ atomic_uintmax_t; 
+
 
 #endif /* _STDATOMIC_H */

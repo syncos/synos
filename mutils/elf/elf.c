79919d983532ea27a64a5619115a08844b4e988f
Jacob Paul <jacob_paul@outlook.com>
2019-11-12 12:43:40 +0100

    Added FileIsELF check in ParseELF

--- mutils/elf/elf.c
+++ mutils/elf/elf.c
@@ -4,6 +4,8 @@
 
 struct ELF_OBJ *ParseELF(const char* file, const size_t len)
 {
+    if(!FileIsELF(file)) return NULL;
+
     struct ELF_OBJ *obj = (struct ELF_OBJ*)malloc(sizeof(struct ELF_OBJ));
 
     obj->FILE_LENGTH = len;
@@ -11,7 +13,7 @@ struct ELF_OBJ *ParseELF(const char* file, const size_t len)
     obj->FILE = (char*)malloc(len);
     memcpy(obj->FILE, file, len);
 
-    obj->header = ParseELF_HEADER(file);
+    obj->header = ParseELF_HEADER(obj->FILE);
 
     return obj;
 }

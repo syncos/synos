01f295489e5c3e9c187d7fda9e17dd5e17282fa4
64epicks <jacob_paul@outlook.com>
2019-12-31 16:05:41 +0100

    Added stacktrace name support for multiboot2

--- arch/X86_64/multiboot2.c
+++ arch/X86_64/multiboot2.c
@@ -5,11 +5,40 @@
 #include "x64.h"
 #include "multiboot2.h"
 
+extern uint32_t mbm;
+extern uint32_t mbp;
+
+extern void tab_search();
+
 int mboot2Init()
 {
+    if (mbm != MULTIBOOT2_BOOTLOADER_MAGIC)
+        panic("System not booted with multiboot2 standard. mboot2Init() still invoked (possible kernel bug).");
     load_sys = MULTIBOOT2;
 
-    // TODO: add support for multiboot2 as vast as multiboot
+    X64.symbols.elf_sym = NULL;
+    X64.symbols.elf_str = NULL;
+
+    X64.symbols.elf_sym_size = 0;
+    X64.symbols.elf_str_size = 0;
+
+    X64.sections.elf_sh = NULL;
+    X64.sections.elf_sh_length = 0;
+
+    struct multiboot2_tag *tag;
+    for (tag = (struct multiboot2_tag*)(uintptr_t)(mbp + 8); tag->type != MULTIBOOT2_TAG_TYPE_END; tag = (struct multiboot2_tag *) ((multiboot2_uint8_t *) tag + ((tag->size + 7) & ~7)))
+    {
+        switch (tag->type)
+        {
+            case MULTIBOOT2_TAG_TYPE_ELF_SECTIONS:
+            {
+                X64.sections.elf_sh = (struct ELF64_Shdr *)((struct multiboot2_tag_elf_sections*)tag)->sections;
+                X64.sections.elf_sh_length = ((struct multiboot2_tag_elf_sections*)tag)->num;
+                tab_search();
+                break;
+            }
+        }
+    }
 
     return 0;
 }
